a7b727b4f1cd05460726c200bb7bc3872681d5ff
h4roru 4zure <h4roru4zure@outlook.com>
2023-02-15 00:03:18 -0300

    penultimo commit ... flojera de hacer el resto too easy

---
+++ src/hoisting.js/index.js
@@ -0,0 +1,11 @@
+//console.log(nameOfDog);
+const fruits = () => {
+     if (true) { 
+        var fruit1 = 'apple'; const fruit2 = 'banana'; let fruit3 = 'kiwi'; 
+        console.log(fruit1);
+        console.log(fruit2);
+    }
+    
+    console.log(fruit3);
+} 
+fruits();
---
+++ src/scope/strict.js
@@ -0,0 +1,11 @@
+'use strict';
+var pi = 3.1416;
+console.log(pi);
+
+function myFunction() {
+'use strict';
+   let pi = 3.1416;
+   return pi;
+}
+
+console.log(myFunction());
